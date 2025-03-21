e7a8a8656c30c8f0e5676b05193c0e3e9ba0aab7
JUNG99 <kinggodjung9@naver.com>
2025-03-21 10:27:14 +0900

    [feat] 적생성로직
    -플레이어위치를 기준으로 일정거리마다 생성

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab; //에너미프리팹
+    public Transform player; //플레이어위치
+    public float spawnDistance = 5f; //적생성거리
+    public float spawnInterval = 3f; //적생성주기
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 1f , spawnInterval);  
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 spawnPos = player.position + Vector3.forward * spawnDistance;
+        Instantiate(enemyPrefab,spawnPos,Quaternion.identity);
+        
+    }
+
+    
+
+}
