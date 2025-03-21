3197f8fd6787e22d1294e1725d1b96f48264b732
JUNG99 <kinggodjung9@naver.com>
2025-03-21 11:45:04 +0900

    [feat] 체력시스템
    - 오브젝트에서 사용가능한 체력시스템
    - 체력이 0이되면 사망, 최대체력은 100

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heaㅣth : MonoBehaviour
+{
+    public float maxHealth = 100f; //최대체력
+    public float currentHealth; //현재체럭
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+            
+    }
+
+    public void  TakeDamage(float damage)
+    {
+        currentHealth -= +damage;
+        Debug.Log(gameObject.name + "체력" + currentHealth);
+
+        if (currentHealth < 0)
+        {
+            Die();
+        }
+
+    }
+
+    void Die()
+    {
+        Debug.Log(gameObject.name + "사망");
+        Destroy(gameObject); //죽은 대상 제거
+    }
+    
+
+}
